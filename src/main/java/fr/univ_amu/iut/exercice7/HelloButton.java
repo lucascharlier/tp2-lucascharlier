7483d16b5e99a4c6ced5a52f20cf1fdfe2305548
lucas <lucass.charlier@gmail.com>
2021-06-11 20:14:26 +0200

    exo 7

--- src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
+++ src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
@@ -2,10 +2,22 @@ package fr.univ_amu.iut.exercice7;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 
 public class HelloButton extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Button myButton = new Button("Hello !");
+        myButton.setId("buttonHello");
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(myButton);
+
+        primaryStage.setTitle("Hello !");
+        primaryStage.setScene(new Scene(borderPane,248,71));
+        primaryStage.show();
     }
 }
