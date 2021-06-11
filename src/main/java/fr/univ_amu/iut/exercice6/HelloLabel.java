ef8101491471cf83601c9d4c479fd9f46c5b94c7
lucas <lucass.charlier@gmail.com>
2021-06-11 20:14:18 +0200

    exo 6

--- src/main/java/fr/univ_amu/iut/exercice6/HelloLabel.java
+++ src/main/java/fr/univ_amu/iut/exercice6/HelloLabel.java
@@ -1,11 +1,23 @@
 package fr.univ_amu.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloLabel extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Label label1 = new Label ("Hello !");
+        label1.setId("labelhello");
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(label1);
+
+        primaryStage.setTitle("Hello !");
+        primaryStage.setScene(new Scene(borderPane, 250, 100));
+        primaryStage.show();
     }
 }
